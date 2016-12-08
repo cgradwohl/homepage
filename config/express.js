8ec00770cdc95c90ea6f637356c6077fa2f92388
Chris Gradwohl <christophergradwohl@gmail.com>
2016-12-08 15:33:11 -0800

    added ejs configuration to ./config/express.js

--- config/express.js
+++ config/express.js
@@ -1,6 +1,6 @@
 /*
 ***************************************************************
-express.js
+./config/express.js
 ***************************************************************
 
 
@@ -31,6 +31,9 @@ module.exports = function() {
     }));
     app.use(bodyParser.json());
     app.use(methodOverride());
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
 
     require('../app/routes/index.server.routes.js')(app);
     return app;
