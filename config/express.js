a1c96cd97b98cdb392418615e76ca1514377a335
Chris Gradwohl <christophergradwohl@gmail.com>
2016-12-08 23:30:33 -0800

    comments

--- config/express.js
+++ config/express.js
@@ -4,7 +4,7 @@
 ***************************************************************
 
 
-- configures the Express application
+- configures the Express server application module 
 ***************************************************************
 */
 
@@ -21,17 +21,24 @@ module.exports = function() {
     var app = express();
 
     if( process.env.NODE_ENV==='development' ){
+        // Concise output colored by response status for development use.
+        // The :status token will be colored red for server error codes,
+        // yellow for client error codes, cyan for redirection codes, and
+        // uncolored for all other codes.
         app.use(morgan('dev'));
 
     } else if( process.env.NODE_ENV==='production' ){
         app.use(compress());
     }
 
-
+    // Parse incoming request bodies in a middleware before your handlers,
+    // availabe under the req.body property.
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(bodyParser.json());
+    // Lets you use HTTP verbs such as PUT or DELETE in places where the
+    // client doesn't support it.
     app.use(methodOverride());
 
     /*app.use(session({
@@ -43,6 +50,7 @@ module.exports = function() {
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
 
+    // the routing module function accepts app as argument
     require('../app/routes/index.server.routes.js')(app);
 
     app.use(express.static('./public'));
