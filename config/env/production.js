23ae71eba0eeb940246d02df70c25a80acf28682
Chris Gradwohl <christophergradwohl@gmail.com>
2016-12-08 19:48:43 -0800

    added production.js for heroku error

---
+++ config/env/production.js
@@ -0,0 +1,13 @@
+/*
+***************************************************************
+app/config/env/production.js
+***************************************************************
+
+
+- enviroment configuration file for the 'production' enviroment
+***************************************************************
+*/
+
+module.exports = {
+    //sessionSecret: 'developmentSessionSecret'
+};
