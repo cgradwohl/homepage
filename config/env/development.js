da97be7da1ee3968aad175c9ee3e4bc5a213660c
Chris Gradwohl <christophergradwohl@gmail.com>
2016-12-12 17:37:48 -0800

    used $ heroku config | grep MONGODB_URI to orint URI to terminal and added uri to mongoose

--- config/env/development.js
+++ config/env/development.js
@@ -10,6 +10,6 @@ app/config/env/development.js
 
 module.exports = {
     //db           : 'mongodb://localhost/myapp', // <-- use this for local devolpment
-    db : 'mongodb://heroku_12345678:random_password@ds029017.mLab.com:29017/heroku_12345678'
+    db : 'mongodb://heroku_b4c304kx:1iap0rq9jn0c4eqbc9rahqabbj@ds133388.mlab.com:33388/heroku_b4c304kx'
     sessionSecret: 'developmentSessionSecret'
 };
