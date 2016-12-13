286a109641a4ecaf9aa62c71a96522d17a028730
Chris Gradwohl <christophergradwohl@gmail.com>
2016-12-12 21:13:57 -0800

    manually entered uri

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,10 @@
-var config = require('./config'),
+//var config = require('./config'),
 mongoose   = require('mongoose');
 
 module.exports = function() {
-    var db = mongoose.connect(config);
+    //var db = mongoose.connect(config);
+    var uri = 'mongodb://admin:2chester@ds133428.mlab.com:33428/mean_structure_db';
+    var db = mongoose.connect(uri);
 
     require('../app/models/user.server.model');
 
