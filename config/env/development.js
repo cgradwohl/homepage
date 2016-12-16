375263cce9b0847da39e0e730be44b16e99e1684
Chris Gradwohl <christophergradwohl@gmail.com>
2016-12-15 18:40:44 -0800

    slimed down the apps dependancies

--- config/env/development.js
+++ config/env/development.js
@@ -11,10 +11,5 @@ app/config/env/development.js
 module.exports = {
     db           : 'mongodb://localhost/myapp', // <-- use this for local devolpment
     //db : 'mongodb://admin:2chester@ds133428.mlab.com:33428/mean_structure_db',
-    sessionSecret: 'developmentSessionSecret',
-    facebook: {
-        clientID: '2267073173431666',
-        clientSecret: '86611ca8fb79f0b0570cd0ae3caa846a',
-        callbackURL: 'http://localhost:8080/oauth/facebook/callback'
-    }
+    
 };
