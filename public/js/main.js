a9742d38134d81b23baedfdca1ce28ef633da326
Chris Gradwohl <christophergradwohl@gmail.com>
2016-12-15 20:38:40 -0800

    styleing and added animations

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+$(".proj_btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".projects").offset().top},
+        'fast');
+
+    $('.projects h1').addClass('animated bounceInLeft');
+    $('.projects p').addClass('animated bounceInLeft');
+});
+
+$(".blog_btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".blog").offset().top},
+        'fast');
+
+    $('.blog h1').addClass('animated bounceInRight');
+    $('.blog p').addClass('animated bounceInRight');
+});
+
+$(".hello_btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".sayhello").offset().top},
+        'fast');
+
+    $('.sayhello h1').addClass('animated jello');
+    $('.sayhello p').addClass('animated jello');
+});
